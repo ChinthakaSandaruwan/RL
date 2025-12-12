53c2bf076e7c5031820b84ec57a886a63d9ebb1a
ChinthakaSandaruwan <chinthakasw000@gmail.com>
2025-12-12 15:16:11 +0530

    feat: Add a dynamic navigation bar component and update favicons with a new manifest and image assets.

--- public/navbar/navbar.php
+++ public/navbar/navbar.php
@@ -5,9 +5,7 @@
     <div class="container">
         <!-- Brand on Left -->
         <a class="navbar-brand d-flex align-items-center gap-2" href="<?= app_url('index.php') ?>">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-house-door-fill" viewBox="0 0 16 16">
-                <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.495v3.505a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5z"/>
-            </svg>
+            <img src="<?= app_url('public/favicon/apple-touch-icon.png') ?>" alt="Logo" width="30" height="30" class="d-inline-block align-text-top rounded">
             Rental Lanka
         </a>
 
